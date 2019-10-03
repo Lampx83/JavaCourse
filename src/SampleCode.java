0bc5238d691b1ea94dc11a80bebe2f88b1b003c2
Nguyen Binh Minh <phamxuanlam@gmail.com>
2019-10-03 14:53:45 +0700

    Sample Code

---
+++ src/SampleCode.java
@@ -0,0 +1,6 @@
+
+public class SampleCode {
+    public static void main(String[] args) {
+        System.out.println("We will not use 'Hello, World!'");
+    }
+}
---
+++ src/v1ch03/_1_FirstSample/FirstSample.java
@@ -0,0 +1,7 @@
+package v1ch03._1_FirstSample;
+
+public class FirstSample {
+    public static void main(String[] args) {
+        System.out.println("We will not use 'Hello, World!'");
+    }
+}
